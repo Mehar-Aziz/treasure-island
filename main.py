4874e948951a0f6e543795523ee706f91909ead5
Mehar-Aziz <meharaziz.1614>
2024-07-13 19:57:01 +0500

    Modified Code

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 
-choice1 = input("Your are at a crossroad, where do you want to go? Type 'left' or 'right'. ").lower()
+choice1 = input("Your are at a crossroad, where do you want to go? Type 'left' or 'right'. \n").lower()
 
 if choice1 == "left":
-    choice2 = input("You have come to a lake. There is an island in the middle of the lake, Type 'wait' to wait for the boat or Type 'swim' to swim across. ").lower()
+    choice2 = input("You have come to a lake. There is an island in the middle of the lake, Type 'wait' to wait for the boat or Type 'swim' to swim across. \n").lower()
     if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you want to choose? ").lower()
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you want to choose? \n").lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over!! ")
         elif choice3 == "yellow":
